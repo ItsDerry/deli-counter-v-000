fa4186d00f318e0cf61ec1afffa35d8c77fedd12
Derrick Reyes <derrickreyes@me.com>
2018-08-29 22:45:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -28,6 +28,6 @@ def now_serving(katz_deli)
     puts katz_deli[0]
     katz_deli.shift(0)
   else
-    "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
 end
 end
