3d1bc994351e8b92c1d7ed8fa2828969c6aebbf9
Derrick Reyes <derrickreyes@me.com>
2018-08-29 22:25:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  else
+    line_of_patrons = String.new
+    new_array = Array.new
+    new_array = katz_deli.each_with_index.collect{ |patron, index|
+      "#{index + 1}. #{patron}"
+    }
+    new_string = new_array.join(" ")
+    line_of_patrons = "The line is currently: " + new_string
+    puts line_of_patrons
+  end
+end
+
+def take_a_number(deli, person)
+  if deli == []
+    katz_deli << person
+    puts "Welcome, #{person}. You are number 1 in line."
+  #your_position = Array.new
+  #your_position = katz_deli << "Ada"
+  else
+    deli.each_with_index do |person, index|
+    deli << person
+    returns "Welcome, #{person}, You are number #{index + 1} in line."
+  end
+end
+end
+
+#def now_serving
+#end
